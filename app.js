d3fbbc5c657d9edd48bc8db3a77e14753c34f441
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-05-06 00:37:04 +0000

    post to the db

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get("/", function(req, res) {
 
 app.get("/campgrounds", function(req, res) {
     Campground.find({}, function(err, allCampgrounds) {
-        if (err) {
+        if(err) {
             console.log(err);
         } else {
             res.render("campgrounds", {campgrounds: allCampgrounds});
@@ -61,8 +61,13 @@ app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res) {
