a14e26acce556595fd7bb3b523f6af137d3ca282
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-05-16 15:14:21 +0000

    create 'show' route/add one more parameter to the schema

--- app.js
+++ app.js
@@ -13,13 +13,14 @@ app.set("view engine", "ejs");
 
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
-//     {name: "Granite Hill", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"},
+//     {name: "Granite Hill", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg", description: "This is a huge granite hill, no bathroom, no water. Beautiful granite!"},
 //     function(err, campground){
 //         if(err){
 //             console.log(err);
@@ -28,20 +29,6 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //         }
 //     }
 // );
-
-// 
-
-var campgrounds = [
-    {name: "Salmon Creeks", image: "https://pixabay.com/get/ea36b7062bf6093ed1584d05fb1d4e97e07ee3d21cac104497f5c370a4eeb6b9_340.jpg"},
-    {name: "Granite Hill", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"},
-    {name: "Salmon Creeks", image: "https://pixabay.com/get/ea36b7062bf6093ed1584d05fb1d4e97e07ee3d21cac104497f5c370a4eeb6b9_340.jpg"},
-    {name: "Granite Hill", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"},
-    {name: "Salmon Creeks", image: "https://pixabay.com/get/ea36b7062bf6093ed1584d05fb1d4e97e07ee3d21cac104497f5c370a4eeb6b9_340.jpg"},
-    {name: "Granite Hill", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"}
-]
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -72,6 +59,10 @@ app.post("/campgrounds", function(req, res) {
 
 app.get("/campgrounds/new", function(req, res) {
    res.render("new"); 
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+    res.send("Show page");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
